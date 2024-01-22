6be3baa61e03145bed54e9fe7b258d6f0523c5fa
luna-sprobe <luna.e@sprobe.com>
2024-01-23 00:28:42 +0800

    update examples
    mobile updates for scope and collection examples

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,49 +1,201 @@
+import 'dart:async';
+import 'package:cbl/cbl.dart';
 import 'package:cbl_flutter/cbl_flutter.dart';
-import 'package:example/services/couchbase_service.dart';
 import 'package:flutter/material.dart';
-import 'package:example/view_models/login_view_model.dart';
-import 'package:example/views/login_screen_view.dart';
-import 'package:provider/provider.dart';
+import 'package:intl/intl.dart';
 
+late Database database;
+late Collection chatMessages;
+late ChatMessageRepository chatMessageRepository;
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  //await CouchbaseLiteFlutter.init();
-
-  runApp(const App());
+  await CouchbaseLiteFlutter.init();
+  database = await Database.openAsync('example');
+  chatMessages = await database.createCollection('chatMessages');
+  await chatMessages.createIndex(
+    'type+createdAt',
+    ValueIndex([
+      ValueIndexItem.property('type'),
+      ValueIndexItem.property('createdAt'),
+    ]),
+  );
+  chatMessageRepository = ChatMessageRepository(database, chatMessages);
+  runApp(const MyApp());
 }
-
-class App extends StatefulWidget {
-  const App({super.key});
-
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
   @override
-  State<App> createState() => _AppState();
+  Widget build(BuildContext context) => const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: ChatMessagesPage(),
+      );
 }
-
-class _AppState extends State<App> {
-  late final CouchbaseService _couchbaseService;
-
+class ChatMessagesPage extends StatefulWidget {
+  const ChatMessagesPage({super.key});
+  @override
+  State<ChatMessagesPage> createState() => _ChatMessagesPageState();
+}
+class _ChatMessagesPageState extends State<ChatMessagesPage> {
+  List<ChatMessage> _chatMessages = [];
+  late StreamSubscription _chatMessagesSub;
   @override
   void initState() {
     super.initState();
-    _couchbaseService = CouchbaseService();
+    _chatMessagesSub =
+        chatMessageRepository.allChatMessagesStream().listen((chatMessages) {
+      setState(() => _chatMessages = chatMessages);
+    });
   }
-
   @override
-  Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(
-          create: (_) => LoginViewModel(couchbaseService: _couchbaseService),
+  void dispose() {
+    _chatMessagesSub.cancel();
+    super.dispose();
+  }
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        body: SafeArea(
+          child: Column(children: [
+            Expanded(
+              child: ListView.builder(
+                reverse: true,
+                itemCount: _chatMessages.length,
+                itemBuilder: (context, index) {
+                  final chatMessage =
+                      _chatMessages[_chatMessages.length - 1 - index];
+                  return ChatMessageTile(chatMessage: chatMessage);
+                },
+              ),
+            ),
+            const Divider(height: 0),
+            _ChatMessageForm(onSubmit: chatMessageRepository.createChatMessage)
+          ]),
         ),
-      ],
-      child: MaterialApp(
-        title: 'Couchbase Lite Example',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
-          useMaterial3: true,
+      );
+}
+class ChatMessageTile extends StatelessWidget {
+  const ChatMessageTile({super.key, required this.chatMessage});
+  final ChatMessage chatMessage;
+  @override
+  Widget build(BuildContext context) => Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              DateFormat.yMd().add_jm().format(chatMessage.createdAt),
+              style: Theme.of(context).textTheme.bodySmall,
+            ),
+            const SizedBox(height: 5),
+            Text(chatMessage.message)
+          ],
         ),
-        home: const LoginPage(),
-      ),
-    );
+      );
+}
+class _ChatMessageForm extends StatefulWidget {
+  const _ChatMessageForm({required this.onSubmit});
+  final ValueChanged<String> onSubmit;
+  @override
+  _ChatMessageFormState createState() => _ChatMessageFormState();
+}
+class _ChatMessageFormState extends State<_ChatMessageForm> {
+  late final TextEditingController _messageController;
+  late final FocusNode _messageFocusNode;
+  @override
+  void initState() {
+    super.initState();
+    _messageController = TextEditingController();
+    _messageFocusNode = FocusNode();
+  }
+  @override
+  void dispose() {
+    _messageController.dispose();
+    _messageFocusNode.dispose();
+    super.dispose();
+  }
+  void _onSubmit() {
+    final message = _messageController.text.trim();
+    if (message.isEmpty) {
+      return;
+    }
+    widget.onSubmit(message);
+    _messageController.clear();
+    _messageFocusNode.requestFocus();
+  }
+  @override
+  Widget build(BuildContext context) => Padding(
+        padding: const EdgeInsets.all(20),
+        child: Row(
+          children: [
+            Expanded(
+              child: TextField(
+                decoration:
+                    const InputDecoration.collapsed(hintText: 'Message'),
+                autofocus: true,
+                focusNode: _messageFocusNode,
+                controller: _messageController,
+                minLines: 1,
+                maxLines: 10,
+                style: Theme.of(context).textTheme.bodyMedium,
+                textCapitalization: TextCapitalization.sentences,
+              ),
+            ),
+            const SizedBox(width: 5),
+            TextButton(
+              onPressed: _onSubmit,
+              child: const Text('Send'),
+            )
+          ],
+        ),
+      );
+}
+abstract class ChatMessage {
+  String get id;
+  String get message;
+  DateTime get createdAt;
+}
+class CblChatMessage extends ChatMessage {
+  CblChatMessage(this.dict);
+  final DictionaryInterface dict;
+  @override
+  String get id => dict.documentId;
+  @override
+  DateTime get createdAt => dict.value('createdAt')!;
+  @override
+  String get message => dict.value('message')!;
+}
+extension DictionaryDocumentIdExt on DictionaryInterface {
+  String get documentId {
+    final self = this;
+    return self is Document ? self.id : self.value('id')!;
+  }
+}
+class ChatMessageRepository {
+  ChatMessageRepository(this.database, this.collection);
+  final Database database;
+  final Collection collection;
+  Future<ChatMessage> createChatMessage(String message) async {
+    final doc = MutableDocument({
+      'type': 'chatMessage',
+      'createdAt': DateTime.now(),
+      'message': message,
+    });
+    await collection.saveDocument(doc);
+    return CblChatMessage(doc);
+  }
+  Stream<List<ChatMessage>> allChatMessagesStream() {
+    final query = const QueryBuilder()
+        .select(
+          SelectResult.expression(Meta.id),
+          SelectResult.property('createdAt'),
+          SelectResult.property('message'),
+        )
+        .from(DataSource.collection(collection))
+        .where(
+          Expression.property('type').equalTo(Expression.value('chatMessage')),
+        )
+        .orderBy(Ordering.property('createdAt'));
+    return query.changes().asyncMap(
+          (change) => change.results.asStream().map(CblChatMessage.new).toList(),
+        );
   }
 }

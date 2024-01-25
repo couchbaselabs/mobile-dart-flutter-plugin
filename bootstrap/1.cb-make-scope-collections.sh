5b11d1e613191f1fd0160f62ed53df63db8b82f9
Fujio Turner <Fujio-Turner@users.noreply.github.com>
2024-01-25 15:14:51 -0600

    conflict cleanup

--- bootstrap/1.cb-make-scope-collections.sh
+++ bootstrap/1.cb-make-scope-collections.sh
@@ -5,15 +5,9 @@ username="Administrator"
 password="password"
 cbBucket="message" 
 cbScope="chat"
-<<<<<<< Updated upstream
-cbHost="localhost:8091"
-sgDb="examplechat"
-sgAdminHost="localhost:4985"
-=======
 cbCollections=("message")
 sgRbacUser="syncGatewayUser"
 sgRbacUserPass="password"
->>>>>>> Stashed changes
 vb=" -v "
 
 
@@ -25,12 +19,6 @@ sleep 10
 curl -u $username:$password $vb -X POST http://$cbHost/pools/default/buckets/$cbBucket/scopes -d name=$cbScope
 sleep 2
 
-<<<<<<< Updated upstream
-# Define the list of collection names
-collections=("messages")
-
-=======
->>>>>>> Stashed changes
 # Iterate through the collections and make the corresponding API calls
 for cbCollection in "${cbCollections[@]}"
 do

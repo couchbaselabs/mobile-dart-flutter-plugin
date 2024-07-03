deb0bfabfc8a6d7e6922adaeae2b63dd2c998a45
Jeff-Couchbase <jeffrey.milanes@couchbase.com>
2024-07-03 21:44:46 +0800

    update example

--- example/lib/chat.dart
+++ example/lib/chat.dart
@@ -6,122 +6,70 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
+// update this with your device ip
+const String cbUrl = 'ws://192.168.0.183:4984/examplechat';
+const String databaseName = 'examplechat';
+const String scope = 'chat';
+const String collection = 'message';
+const String username = 'bob';
+const String password = '12345';
+
 class ChatMessagesPage extends StatefulWidget {
   const ChatMessagesPage({super.key});
-
   @override
   State<ChatMessagesPage> createState() => _ChatMessagesPageState();
 }
 
 class _ChatMessagesPageState extends State<ChatMessagesPage> {
-  List<dynamic> webMessages = [];
+  
+
+  @override
+  Widget build(BuildContext context) => kIsWeb ? const ChatMessagePageWeb() : const ChatMessagePageMobile();
+}
+
+class ChatMessagePageWeb extends StatefulWidget {
+  const ChatMessagePageWeb({super.key});
+
+  @override
+  State<ChatMessagePageWeb> createState() => _ChatMessagePageWebState();
+}
+
+class _ChatMessagePageWebState extends State<ChatMessagePageWeb> {
+   final CblWebSocket _cblWebSocket = CblWebSocket();
   final ScrollController _scrollController = ScrollController();
+
+  List<dynamic> messages = [];
 
   @override
   void initState() {
     super.initState();
-  }
+    _cblWebSocket.createCollection(collection, scope);
 
-  Future<ChatMessageRepository> setup() async {
-    late Database database;
-    late Collection chatMessages;
-    late Replicator replicator;
-    late ChatMessageRepository chatMessageRepository;
+    _cblWebSocket.connect(
+        url: cbUrl,
+        username: username,
+        password: password, channels: []);
 
-    database = await Database.openAsync('examplechat');
+    _cblWebSocket.startListening((message) {
+      if ((message != null || message != '') && message is String) {
+        List<dynamic> decodedMsg = json.decode(message);
 
-    chatMessages = await database.createCollection('message', 'chat');
-
-    // update this with your device ip
-    final targetURL = Uri.parse('ws://192.168.0.116:4984/examplechat');
-
-    final targetEndpoint = UrlEndpoint(targetURL);
-
-    final config = ReplicatorConfiguration(target: targetEndpoint);
-
-    config.replicatorType = ReplicatorType.pushAndPull;
-
-    config.enableAutoPurge = false;
-
-    config.continuous = true;
-
-    config.authenticator =
-        BasicAuthenticator(username: "bob", password: "12345");
-
-    config.addCollection(chatMessages);
-
-    replicator = await Replicator.create(config);
-
-    replicator.addChangeListener((change) {
-      if (change.status.activity == ReplicatorActivityLevel.stopped) {
-        print('Replication stopped');
-      } else {
-        print('Replicator is currently: ${change.status.activity.name}');
-      }
-
-      if (kIsWeb) {
-        print("Web Data: ${change.status.webData}");
-
-        if (change.status.webData != null &&
-            change.status.webData != '' &&
-            change.status.webData is String) {
-          List<String> decodedMsg = json.decode(change.status.webData ?? '');
-
-          setState(() {
-            webMessages.addAll(decodedMsg);
-          });
-        }
-
-        // for scrolling effect in web
+        setState(() {
+          messages.addAll(decodedMsg);
+        });
 
         WidgetsBinding.instance.addPostFrameCallback((_) {
           if (_scrollController.hasClients) {
             _scrollController
                 .jumpTo(_scrollController.position.maxScrollExtent);
           } else {
-            setState(() {});
+            setState(() => {});
           }
         });
       }
     });
-
-    await replicator.start();
-
-// for web to pass the replicator configuration back to the collection
-    chatMessages.replicatorConfig(config);
-    chatMessageRepository = ChatMessageRepository(database, chatMessages);
-
-    return chatMessageRepository;
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: kIsWeb
-          ? ChatMessagesPageWeb(webMessages: webMessages)
-          : FutureBuilder<ChatMessageRepository>(
-              future: setup(),
-              builder: (context, snapshot) => snapshot.data == null
-                  ? const Center(child: CircularProgressIndicator())
-                  : ChatMessagesPageMobile(
-                      repository: snapshot.data,
-                    )),
-    );
-  }
-}
-
-class ChatMessagesPageWeb extends StatefulWidget {
-  final ScrollController? scrollController;
-  final List<dynamic>? webMessages;
-
-  const ChatMessagesPageWeb(
-      {this.scrollController, this.webMessages, super.key});
-
-  @override
-  State<ChatMessagesPageWeb> createState() => _ChatMessagesPageWebState();
-}
-
-class _ChatMessagesPageWebState extends State<ChatMessagesPageWeb> {
   @override
   Widget build(BuildContext context) => Scaffold(
         body: SafeArea(
@@ -129,10 +77,10 @@ class _ChatMessagesPageWebState extends State<ChatMessagesPageWeb> {
             Expanded(
               child: ListView.builder(
                 reverse: false,
-                controller: widget.scrollController,
-                itemCount: widget.webMessages?.length,
+                controller: _scrollController,
+                itemCount: messages.length,
                 itemBuilder: (context, index) {
-                  final item = widget.webMessages?[index];
+                  final item = messages[index];
                   return ChatMessageTileWeb(
                     message: item.containsKey('doc')
                         ? item['doc']['chatMessage']
@@ -146,76 +94,10 @@ class _ChatMessagesPageWebState extends State<ChatMessagesPageWeb> {
               ),
             ),
             const Divider(height: 0),
-            _ChatMessageForm(
+            _ChatMessageFormWeb(
               onSubmit: (message) {},
+              cblWebSocket: _cblWebSocket,
             )
-          ]),
-        ),
-      );
-}
-
-class ChatMessagesPageMobile extends StatefulWidget {
-  const ChatMessagesPageMobile({this.repository, super.key});
-  final ChatMessageRepository? repository;
-
-  @override
-  State<ChatMessagesPageMobile> createState() => _ChatMessagesPageMobileState();
-}
-
-class _ChatMessagesPageMobileState extends State<ChatMessagesPageMobile> {
-  List<ChatMessage> _chatMessages = [];
-  late StreamSubscription _chatMessagesSub;
-  final CblPerformanceLogger _cblPerformanceLogger = CblPerformanceLogger();
-  int count = 0;
-
-  @override
-  void initState() {
-    super.initState();
-    _cblPerformanceLogger.start('wsPerformance');
-
-    _chatMessagesSub =
-        widget.repository!.allChatMessagesStream().listen((chatMessages) {
-      setState(() {
-        _chatMessages = chatMessages;
-        count = count + 1;
-        _cblPerformanceLogger.end('wsPerformance');
-        print(count);
-      });
-    });
-  }
-
-  @override
-  void dispose() {
-    _chatMessagesSub.cancel();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) => Scaffold(
-        body: SafeArea(
-          child: Column(children: [
-            Container(
-              padding: const EdgeInsets.all(20),
-              color: Theme.of(context).primaryColor,
-              child: Center(
-                child: Text(
-                  'Chat Count: ${_chatMessages.length}',
-                ),
-              ),
-            ),
-            Expanded(
-              child: ListView.builder(
-                reverse: true,
-                itemCount: _chatMessages.length,
-                itemBuilder: (context, index) {
-                  final chatMessage =
-                      _chatMessages[_chatMessages.length - 1 - index];
-                  return ChatMessageTileMobile(chatMessage: chatMessage);
-                },
-              ),
-            ),
-            const Divider(height: 0),
-            _ChatMessageForm(onSubmit: widget.repository!.createChatMessage)
           ]),
         ),
       );
@@ -244,8 +126,207 @@ class ChatMessageTileWeb extends StatelessWidget {
       );
 }
 
-class ChatMessageTileMobile extends StatelessWidget {
-  const ChatMessageTileMobile({super.key, required this.chatMessage});
+class _ChatMessageFormWeb extends StatefulWidget {
+  const _ChatMessageFormWeb({
+    required this.cblWebSocket,
+    required this.onSubmit,
+  });
+  final CblWebSocket cblWebSocket;
+  final ValueChanged<String> onSubmit;
+  @override
+  _ChatMessageFormWebState createState() => _ChatMessageFormWebState();
+}
+
+class _ChatMessageFormWebState extends State<_ChatMessageFormWeb> {
+  late final TextEditingController _messageController;
+  late final FocusNode _messageFocusNode;
+
+  @override
+  void initState() {
+    super.initState();
+    _messageController = TextEditingController();
+    _messageFocusNode = FocusNode();
+  }
+
+  @override
+  void dispose() {
+    _messageController.dispose();
+    _messageFocusNode.dispose();
+    super.dispose();
+  }
+
+  void _onSubmit() {
+    final message = _messageController.text.trim();
+    if (message.isEmpty) {
+      return;
+    }
+
+    Map<String, Object> doc = {
+      'type': 'chatMessage',
+      'createdAt':
+          DateFormat("yyyy-MM-ddTHH:mm:ss.SSSSSS").format(DateTime.now()),
+      'userId': 'bob',
+      'chatMessage': message,
+    };
+
+    widget.cblWebSocket.saveDocument(doc);
+
+    widget.onSubmit(message);
+    _messageController.clear();
+    _messageFocusNode.requestFocus();
+  }
+
+  @override
+  Widget build(BuildContext context) => Padding(
+        padding: const EdgeInsets.all(20),
+        child: Row(
+          children: [
+            Expanded(
+              child: TextField(
+                decoration:
+                    const InputDecoration.collapsed(hintText: 'Message'),
+                autofocus: true,
+                focusNode: _messageFocusNode,
+                controller: _messageController,
+                minLines: 1,
+                maxLines: 10,
+                style: Theme.of(context).textTheme.bodyMedium,
+                textCapitalization: TextCapitalization.sentences,
+              ),
+            ),
+            const SizedBox(width: 5),
+            TextButton(
+              onPressed: _onSubmit,
+              child: const Text('Send'),
+            )
+          ],
+        ),
+      );
+}
+
+class ChatMessagePageMobile extends StatefulWidget {
+  const ChatMessagePageMobile({super.key});
+
+  @override
+  State<ChatMessagePageMobile> createState() => _ChatMessagePageMobileState();
+}
+
+class _ChatMessagePageMobileState extends State<ChatMessagePageMobile> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  Future<ChatMessageRepository> setup() async {
+    late Database database;
+    late Collection chatMessages;
+    late Replicator replicator;
+    late ChatMessageRepository chatMessageRepository;
+
+    database = await Database.openAsync(databaseName);
+
+    chatMessages = await database.createCollection(collection, scope);
+
+    final targetURL = Uri.parse(cbUrl);
+
+    final targetEndpoint = UrlEndpoint(targetURL);
+
+    final config = ReplicatorConfiguration(target: targetEndpoint);
+
+    config.replicatorType = ReplicatorType.pushAndPull;
+
+    config.enableAutoPurge = false;
+
+    config.continuous = true;
+
+    config.authenticator =
+        BasicAuthenticator(username: "bob", password: "12345");
+
+    config.addCollection(chatMessages, CollectionConfiguration());
+
+    replicator = await Replicator.create(config);
+
+    replicator.addChangeListener((change) {
+      if (change.status.activity == ReplicatorActivityLevel.stopped) {
+        print('Replication stopped');
+      } else {
+        print('Replicator is currently: ${change.status.activity.name}');
+      }
+    });
+
+    await replicator.start();
+
+    chatMessageRepository = ChatMessageRepository(database, chatMessages);
+
+    return chatMessageRepository;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: FutureBuilder<ChatMessageRepository>(
+          future: setup(),
+          builder: (context, snapshot) => snapshot.data == null
+              ? const Center(child: CircularProgressIndicator())
+              : ChatMessagesPageMobile(
+                  repository: snapshot.data,
+                )),
+    );
+  }
+}
+
+class ChatMessagesPageMobile extends StatefulWidget {
+  const ChatMessagesPageMobile({this.repository, super.key});
+  final ChatMessageRepository? repository;
+
+  @override
+  State<ChatMessagesPageMobile> createState() => _ChatMessagesPageMobileState();
+}
+
+class _ChatMessagesPageMobileState extends State<ChatMessagesPageMobile> {
+  List<ChatMessage> _chatMessages = [];
+  late StreamSubscription _chatMessagesSub;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _chatMessagesSub =
+        widget.repository!.allChatMessagesStream().listen((chatMessages) {
+      setState(() => _chatMessages = chatMessages);
+    });
+  }
+
+  @override
+  void dispose() {
+    _chatMessagesSub.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        body: SafeArea(
+          child: Column(children: [
+            Expanded(
+              child: ListView.builder(
+                reverse: true,
+                itemCount: _chatMessages.length,
+                itemBuilder: (context, index) {
+                  final chatMessage =
+                      _chatMessages[_chatMessages.length - 1 - index];
+                  return ChatMessageTile(chatMessage: chatMessage);
+                },
+              ),
+            ),
+            const Divider(height: 0),
+            _ChatMessageForm(onSubmit: widget.repository!.createChatMessage)
+          ]),
+        ),
+      );
+}
+
+class ChatMessageTile extends StatelessWidget {
+  const ChatMessageTile({super.key, required this.chatMessage});
   final ChatMessage chatMessage;
   @override
   Widget build(BuildContext context) => Padding(
@@ -337,10 +418,8 @@ class CblChatMessage extends ChatMessage {
   final DictionaryInterface dict;
   @override
   String get id => dict.documentId;
-
   @override
   DateTime get createdAt => dict.value('createdAt')!;
-
   @override
   String get chatMessage => dict.value('chatMessage') ?? '-';
 }
@@ -369,28 +448,23 @@ class ChatMessageRepository {
   }
 
   Stream<List<ChatMessage>> allChatMessagesStream() {
-    // this is needed because this is not available currently in web since we don't have
-    // database that handles the data of the web.
-    if (!kIsWeb) {
-      final query = const QueryBuilder()
-          .select(
-            SelectResult.expression(Meta.id),
-            SelectResult.property('createdAt'),
-            SelectResult.property('chatMessage'),
-          )
-          .from(DataSource.collection(collection))
-          .where(Expression.property('type')
-              .equalTo(Expression.value('chatMessage')))
-          .orderBy(Ordering.property('createdAt'));
+    final query = const QueryBuilder()
+        .select(
+          SelectResult.expression(Meta.id),
+          SelectResult.property('createdAt'),
+          SelectResult.property('chatMessage'),
+        )
+        .from(DataSource.collection(collection))
+        .where(
+          Expression.property('type').equalTo(Expression.value('chatMessage')),
+        )
+        .orderBy(Ordering.property('createdAt'));
 
-      return query.changes().asyncMap(
-            (change) => change.results
-                .asStream()
-                .map((result) => CblChatMessage(result))
-                .toList(),
-          );
-    } else {
-      return const Stream.empty();
-    }
+    return query.changes().asyncMap(
+          (change) => change.results
+              .asStream()
+              .map((result) => CblChatMessage(result))
+              .toList(),
+        );
   }
 }
